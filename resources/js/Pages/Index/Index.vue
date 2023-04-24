7951d1946a2d68e1145adae9a1e4172d50f85f20
LaupWing <loc_strict@hotmail.com>
2023-04-24 07:56:36 +0200

    udpate

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -1,9 +1,21 @@
 <template>
    <div>
+      <header>
+         <nav>
+            <ul>
+               <li>
+                  <Link href="/">index</Link>
+                  <Link href="/show">Show</Link>
+               </li>
+            </ul>
+         </nav>
+      </header>
       <h1>Test</h1>
    </div>
 </template>
 
 <script setup lang="ts">
+import { Link } from "@inertiajs/vue3"
+
 
 </script>
--- resources/js/Pages/Index/Show.vue
+++ resources/js/Pages/Index/Show.vue
@@ -1,9 +1,20 @@
 <template>
    <div>
+      <header>
+         <nav>
+            <ul>
+               <li>
+                  <Link href="/">index</Link>
+                  <Link href="/show">Show</Link>
+               </li>
+            </ul>
+         </nav>
+      </header>
       Show page
    </div>
 </template>
 
 <script setup lang="ts">
+import { Link } from "@inertiajs/vue3"
 
 </script>
