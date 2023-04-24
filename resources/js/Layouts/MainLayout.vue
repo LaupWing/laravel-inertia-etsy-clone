c3a5367ce602819f131469eb0c4f7fb235c65aaf
LaupWing <loc_strict@hotmail.com>
2023-04-24 07:59:26 +0200

    main laoyout

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,19 @@
+<template>
+   <div>
+      <header>
+         <nav>
+            <ul>
+               <li>
+                  <Link href="/">index</Link>
+                  <Link href="/show">Show</Link>
+               </li>
+            </ul>
+         </nav>
+      </header>
+      <slot />
+   </div>
+</template>
+
+<script setup lang="ts">
+import { Link } from "@inertiajs/vue3"
+</script>
