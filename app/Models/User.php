3c1bc77bf3e5fffaea9871d1c0154e85990ece32
LaupWing <loc_strict@hotmail.com>
2023-04-24 09:48:54 +0200

    udpate

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,12 @@ class User extends Authenticatable
    protected $casts = [
       'email_verified_at' => 'datetime',
    ];
+
+   public function shops() {
+      return $this->hasMany(Shop::class);
+   }
+
+   public function orders() {
+      return $this->hasMany(Order::class);
+   }
 }
