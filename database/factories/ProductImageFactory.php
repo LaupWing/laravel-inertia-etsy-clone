a01fac20b8242c5e4b565048bd4becc3148a575b
LaupWing <loc_strict@hotmail.com>
2023-04-25 09:09:54 +0200

    update

--- database/factories/ProductImageFactory.php
+++ database/factories/ProductImageFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -19,7 +20,7 @@ class ProductImageFactory extends Factory
       return [
          "url" => fake()->imageUrl(),
          "product_id" => function() {
-            
+            return Product::inRandomOrder()->first()->id;
          }
       ];
    }
