1e54dda24d247891b9971eee8f218b91c9bca76c
LaupWing <loc_strict@hotmail.com>
2023-04-25 11:21:58 +0200

    udpate

--- resources/js/Pages/Shop/Create.vue
+++ resources/js/Pages/Shop/Create.vue
@@ -1,7 +1,10 @@
 <template>
    <div>
       <form action="">
-         
+         <input type="file">
+         <input type="text">
+         <input type="text">
+         <button>create</button>
       </form>
    </div>
 </template>
