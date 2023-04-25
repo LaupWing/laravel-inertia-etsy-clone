31450cb41b7f64adc6c5908a9a0c9317c20e6ef8
LaupWing <loc_strict@hotmail.com>
2023-04-25 10:14:41 +0200

    got it

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,13 @@ class DatabaseSeeder extends Seeder
         \App\Models\User::factory(10)->create();
         \App\Models\Shop::factory(10)->create();
         \App\Models\Product::factory(20)->create();
-        \App\Models\ProductImage::factory(50)->create();
+        $productImages = \App\Models\ProductImage::factory(50)->make();
+        foreach ($productImages as $key => $productImage) {
+            $product = $productImage->product;
+            $lastOrder = $product->images()->max("order") ?? 0;
+            $order = $lastOrder + 1;
+            $productImage->order = $order;
+            $productImage->save();
+         }
     }
 }
