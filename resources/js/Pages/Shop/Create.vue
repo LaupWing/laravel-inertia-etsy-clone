dd5fa40c1b8333459e7d600e195ae43ef720aaae
LaupWing <loc_strict@hotmail.com>
2023-04-25 11:26:19 +0200

    udpate

--- resources/js/Pages/Shop/Create.vue
+++ resources/js/Pages/Shop/Create.vue
@@ -1,14 +1,39 @@
 <template>
    <div>
       <form action="">
-         <input type="file">
-         <input type="text">
-         <input type="text">
+         <div>
+            <input type="file">
+         </div>
+         <div>
+            <label for="name">Name</label>
+            <input 
+               v-model="formData.name"
+               type="text" 
+               id="name"
+            >
+         </div>
+         <div>
+            <label for="description">Description</label>
+            <textarea 
+               v-model="formData.description"
+               name="" 
+               id="description" 
+               cols="30" 
+               rows="10"
+            ></textarea>
+         </div>
          <button>create</button>
       </form>
    </div>
 </template>
 
 <script setup lang="ts">
+import { useForm } from "@inertiajs/vue3"
+
+const formData = useForm({
+   name: "",
+   profile_image: null,
+   description: ""
+})
 
 </script>
