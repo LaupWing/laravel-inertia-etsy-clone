8ccb3872235593a9b9996eb378b64ac9a316a76a
LaupWing <loc_strict@hotmail.com>
2023-04-28 10:32:12 +0200

    added rating

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,16 +1,24 @@
 <template>
-   <div class="bg-red-400">
-      <header>
-         <nav>
-            <ul>
-               <li>
-                  <Link href="/">index</Link>
-               </li>
-               <li>
-                  <Link href="/show">Show</Link>
-               </li>
-            </ul>
-         </nav>
+   <div class="bg-white flex flex-col">
+      <header class="flex flex-col border-b-2 py-2">
+         <div class="flex flex-col mx-auto flex-1 w-full max-w-6xl">
+            <div class="flex pt-2 items-center">
+               <h1 class="text-orange-500 font-bold text-3xl mb-1">Betsy</h1>
+               <div class="flex items-center ml-4">
+                  <input class="px-2 py-1 rounded-full" type="text">
+               </div>
+            </div>
+            <nav>
+               <ul class="flex gap-2">
+                  <li>
+                     <Link href="/">index</Link>
+                  </li>
+                  <li>
+                     <Link href="/show">Show</Link>
+                  </li>
+               </ul>
+            </nav>
+         </div>
       </header>
       <slot />
    </div>
--- resources/js/Pages/Shop/Index.vue
+++ resources/js/Pages/Shop/Index.vue
@@ -1,36 +1,37 @@
 <template>
-   <div>
+   <div class="flex flex-col">
       <div
          v-if="flashSuccess"
       >
          Shop just has been created
       </div>
-      <div
-         v-for="shop in shops"
-         :key="shop.id"
-      >
-         <Link
-            :href="route('shop.edit', {shop: shop.id})"
+      <h2 class="mx-auto text-4xl my-6">Shops Page</h2>
+      <main class="grid grid-cols-2 max-w-5xl w-full gap-4 mx-auto">
+         <section
+            v-for="shop in shops"
+            :key="shop.id"
+            class="flex overflow-hidden grid-cols-1 border-[1.5px] shadow rounded-md"
          >
             <img 
                :src="shop.profile_url" 
-               alt=""
+               :alt="shop.name"
+               class="w-32 aspect-square object-cover"
             >
-         </Link>
-         <Link 
-            :href="route('shop.destroy', {shop: shop.id})"
-            method="delete"
-            as="button"
-         >
-            Delete
-         </Link>
-      </div>
+            <div class="flex flex-col p-4">
+               <h2>{{ shop.name }}</h2>
+               <StarRating read-only :rating="rating"/>
+            </div>
+         </section>
+      </main>
    </div>
 </template>
 
 <script setup lang="ts">
 import { Link, usePage } from "@inertiajs/vue3"
+import { ref } from "vue"
 import { computed } from "vue"
+// @ts-ignore
+import StarRating from "vue-star-rating"
 
 const props = defineProps({
    "shops": Object
@@ -40,5 +41,6 @@ const flashSuccess = computed(() => {
    return page.props?.flash.success
 })
 
+const rating = ref(4)
 console.log(props.shops)
 </script>
