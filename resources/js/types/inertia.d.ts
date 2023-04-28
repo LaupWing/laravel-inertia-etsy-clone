7c5912185027e9ae1229631d25057a2956e54a8e
LaupWing <loc_strict@hotmail.com>
2023-04-28 05:20:11 +0200

    gobalts

--- resources/js/types/inertia.d.ts
+++ resources/js/types/inertia.d.ts
@@ -1,1 +1,23 @@
 export {}
+declare global {
+   export interface User {
+      id: number
+      email: string
+      created_at: Date
+      updated_at: Date
+      name: string
+   }
+
+   export interface Jetstream {
+      [key: string]: boolean
+   }
+
+   export type ErrorBags = undefined | { [key: string]: string[] }
+
+   export type Errors = undefined | string[]
+
+   export type Flash = {
+      message: undefined | string
+      error: undefined | string[]
+   } 
+}
