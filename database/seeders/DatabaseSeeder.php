75246cd0ffdd0b79047e522d52b829dac67c92b9
LaupWing <loc_strict@hotmail.com>
2023-04-30 18:41:18 +0200

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,13 +16,13 @@ class DatabaseSeeder extends Seeder
       \App\Models\Shop::factory(10)->create();
       \App\Models\ShopRating::factory(100)->create();
       \App\Models\Product::factory(20)->create();
-      // $productImages = \App\Models\ProductImage::factory(50)->make();
-      // foreach ($productImages as $productImage) {
-      //    $product = $productImage->product;
-      //    $lastOrder = $product->images()->max("order") ?? 0;
-      //    $order = $lastOrder + 1;
-      //    $productImage->order = $order;
-      //    $productImage->save();
-      // }
+      $productImages = \App\Models\ProductImage::factory(50)->make();
+      foreach ($productImages as $productImage) {
+         $product = $productImage->product;
+         $lastOrder = $product->images()->max("order") ?? 0;
+         $order = $lastOrder + 1;
+         $productImage->order = $order;
+         $productImage->save();
+      }
    }
 }
