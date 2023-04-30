0c120d770669caf67e6ed3a5c687c4d739c4db0b
LaupWing <loc_strict@hotmail.com>
2023-04-30 17:42:04 +0200

    udpate

--- resources/js/Pages/Shop/Components/ShopCard.vue
+++ resources/js/Pages/Shop/Components/ShopCard.vue
@@ -5,19 +5,24 @@
          :alt="shop.name"
          class="w-32 aspect-square object-cover"
       >
-      <div class="flex flex-col p-4">
-         <h2>{{ shop.name }}</h2>
-         <div class="flex items-center">
-            <StarRating 
-               :star-size="20" 
-               read-only 
-               :rating="shop.average_rating"
-               :show-rating="false"
-            />
-            <p class="mt-1 ml-1">({{ shop.ratings_count }})</p>
+      <div class="flex p-4 justify-between flex-1">
+         <div class="flex flex-col">
+            <h2>{{ shop.name }}</h2>
+            <div class="flex items-center">
+               <StarRating 
+                  :star-size="20" 
+                  read-only 
+                  :rating="shop.average_rating"
+                  :show-rating="false"
+               />
+               <p class="mt-1 ml-1">({{ shop.ratings_count }})</p>
+            </div>
+            <div class="mt-auto">
+               {{ shop.products_count }} items
+            </div>
          </div>
-         <div class="mt-auto">
-            {{ shop.products_count }} items
+         <div class="flex items-center ml-auto">
+            <Heart />
          </div>
       </div>
    </section>
@@ -27,6 +32,7 @@
 import { Shop } from "@/types/types"
 // @ts-ignore
 import StarRating from "vue-star-rating"
+import Heart from "../../../Global/Icons/Heart.vue"
 
 defineProps<{
    shop: Shop
