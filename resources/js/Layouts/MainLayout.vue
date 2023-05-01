1dffd482633705adea98cfd7849b29ec09484da3
LaupWing <loc_strict@hotmail.com>
2023-05-01 13:01:18 +0200

    fixed

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -2,11 +2,19 @@
    <div class="bg-white flex flex-col">
       <header class="flex flex-col border-b-2 py-2">
          <div class="flex flex-col mx-auto flex-1 w-full max-w-6xl">
-            <div class="flex pt-2 items-center">
+            <div class="flex pt-2 items-center gap-4">
                <h1 class="text-orange-500 font-bold text-3xl mb-1">Betsy</h1>
-               <div class="flex items-center ml-4">
-                  <input class="px-2 py-1 rounded-full" type="text">
+               <div class="flex items-center flex-1">
+                  <input 
+                     class="px-4 py-1 rounded-full border-gray-400 w-full" 
+                     type="text"
+                     placeholder="Search for..."
+                  >
                </div>
+               <ul class="flex gap-4">
+                  <li>Sign in</li>
+                  <li>Login</li>
+               </ul>
             </div>
             <nav>
                <ul class="flex gap-2">
