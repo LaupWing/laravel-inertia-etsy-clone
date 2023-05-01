a97f46cbf22ed81dbcf3253824082d3d145eb12e
LaupWing <loc_strict@hotmail.com>
2023-05-01 05:42:55 +0200

    udpate

--- resources/js/Pages/Shop/Components/TopShopCard.vue
+++ resources/js/Pages/Shop/Components/TopShopCard.vue
@@ -1,5 +1,5 @@
 <template>
-   <div
+   <section
       class="aspect-square border-2 flex flex-col p-2 border-gray-200 rounded"
    >
       <div class="h-[80%] grid grid-cols-2 grid-rows-2 gap-1 overflow-hidden">
@@ -23,12 +23,28 @@
             class="w-10 h-10 rounded-full"
             :src="shop.profile_url"
          />
+         <div class="flex flex-col h-full ml-2">
+            <h2 class="text-sm">
+               {{ shop.name }}
+            </h2>
+            <div class="flex items-center -mt-1">
+               <StarRating
+                  :star-size="12" 
+                  read-only 
+                  :rating="shop.average_rating"
+                  :show-rating="false"
+               />
+               <p class="mt-1 ml-1 text-xs">({{ shop.ratings_count }})</p>
+            </div>
+         </div>
       </div>
-   </div>
+   </section>
 </template>
 
 <script setup lang="ts">
 import { ShopType } from "@/types/types"
+// @ts-ignore
+import StarRating from "vue-star-rating"
 
 defineProps<{
    shop: ShopType
