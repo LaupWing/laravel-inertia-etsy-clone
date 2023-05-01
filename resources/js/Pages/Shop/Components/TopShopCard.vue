423e62fc62b8602358c00f7006f5d1e4af7b948b
LaupWing <loc_strict@hotmail.com>
2023-05-01 05:12:14 +0200

    created topshopcard

--- resources/js/Pages/Shop/Components/TopShopCard.vue
+++ resources/js/Pages/Shop/Components/TopShopCard.vue
@@ -1,9 +1,28 @@
 <template>
-   <div>
-
+   <div
+      
+      class="aspect-square border-2 flex flex-col p-2 border-gray-200 rounded"
+   >
+      <div class="h-[80%] grid grid-cols-2 grid-rows-2 gap-1 overflow-hidden">
+         <div 
+            class="col-span-1 overflow-hidden"
+            v-for="product in shop.products"
+         >
+            <img 
+               src="https://cms-cdn.placeholder.co/Home_page1_76f0b1d7ab.png?width=1920" 
+               :key="product.id"
+               class="w-full object-cover"
+               alt=""
+            >
+         </div>
+      </div>
    </div>
 </template>
 
 <script setup lang="ts">
+import { ShopType } from "@/types/types"
 
+defineProps<{
+   shop: ShopType
+}>()
 </script>
--- resources/js/Pages/Shop/Index.vue
+++ resources/js/Pages/Shop/Index.vue
@@ -11,25 +11,11 @@
          <section class="flex flex-col gap-4 max-w-5xl w-full mx-auto">
             <h2 class="col-span-full text-2xl">All Shops</h2>
             <div class="grid grid-cols-4 gap-4">
-               <div
+               <TopShopCard
                   v-for="shop in top_shops.splice(0, 4)"
                   :key="shop.id"
-                  class="aspect-square border-2 flex flex-col p-2 border-gray-200 rounded"
-               >
-                  <div class="h-[80%] grid grid-cols-2 grid-rows-2 gap-1 overflow-hidden">
-                     <div 
-                        class="col-span-1 overflow-hidden"
-                        v-for="product in shop.products"
-                     >
-                        <img 
-                           src="https://cms-cdn.placeholder.co/Home_page1_76f0b1d7ab.png?width=1920" 
-                           :key="product.id"
-                           class="w-full object-cover"
-                           alt=""
-                        >
-                     </div>
-                  </div>
-               </div>
+                  :shop="shop"
+               />
             </div>
          </section>
          <section class="flex flex-col gap-4 max-w-5xl w-full mx-auto">
@@ -51,6 +37,7 @@ import type { ShopType } from "@/types/types"
 import { usePage } from "@inertiajs/vue3"
 import { computed } from "vue"
 import ShopCard from "./Components/ShopCard.vue"
+import TopShopCard from "./Components/TopShopCard.vue"
 
 const props = defineProps<{
    shops: ShopType[],
