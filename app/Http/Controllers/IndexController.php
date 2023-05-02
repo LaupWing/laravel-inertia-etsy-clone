8e54ccbb9fdd9389e93ed1dc27a31fd2a8a163ce
LaupWing <loc_strict@hotmail.com>
2023-05-02 15:48:13 +0200

    udpate

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class IndexController extends Controller
 {
    public function index()
    {
+      dd(Auth::user());
       return inertia("Index/Index");
    }
    public function show()
