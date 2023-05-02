f16cd6c921c8fe3cff803eb75b261aff498995aa
LaupWing <loc_strict@hotmail.com>
2023-05-02 15:20:56 +0200

    udpate

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -25,6 +25,8 @@ class AuthController extends Controller
          ]);
       }
       $request->session()->regenerate();
+
+      return redirect()->intended();
    }
 
    public function destroy()
